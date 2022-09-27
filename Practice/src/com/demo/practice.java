53bc839ebfed34e232bee69eb027694d1eeb7368
Prajakta G. Girulkar <Prajakta G. Girulkar@DESKTOP-Q3EFVGA>
2022-09-26 23:26:24 -0700

    new change

--- Practice/src/com/demo/practice.java
+++ Practice/src/com/demo/practice.java
@@ -1,28 +1,27 @@
 package com.demo;
 
 public class practice {
-	static String clg_name="bytr" ;
-    String name = "ram";
-    int String = 101;
-   short  b1 = 32455;
-   static  char s ='g';
-static int clg_id = 101;
+	static String clg_name = "bytr";
+	String name = "ram";
+	int String = 101;
+	short b1 = 32455;
+	static char s = 'g';
+	static int clg_id = 101;
 
-	
-	public static  void main(String[] args) {
+	public static void main(String[] args) {
 		practice p1 = new practice();
 
 		practice p2 = new practice();
-		
+
 		System.out.println(" main method starts");
-		System.out.println(" default value of char = "+s);
-	System.out.println("my clg name = "+clg_name);
-	System.out.println("my name is =  "+p1.b1);
-	
-//		System.out.println("my clg name = "+clg_id);
-//		System.out.println("my name is =  "+p1.name);
-//		System.out.println("my name is =  "+p1.roll_num);
-//		System.out.println("my name is =  "+p1.name);
-//		System.out.println("my name is =  "+p1.roll_num);
+		System.out.println(" default value of char = " + s);
+		System.out.println("my clg name = " + clg_name);
+		System.out.println("my name is =  " + p1.b1);
+
+		// System.out.println("my clg name = "+clg_id);
+		// System.out.println("my name is = "+p1.name);
+		// System.out.println("my name is = "+p1.roll_num);
+		// System.out.println("my name is = "+p1.name);
+		// System.out.println("my name is = "+p1.roll_num);
 	}
 }
--- String Related examples/src/com/string/program/CountVowelConsonant.java
+++ String Related examples/src/com/string/program/CountVowelConsonant.java
@@ -1,40 +1,61 @@
 package com.string.program;
 
-public class CountVowelConsonant {
+import java.util.HashSet;
+import java.util.Set;
 
-	// public static void main(String[] args) {
-	//
-	// System.out.println(stringContainsVowels("Hello")); // true
-	// System.out.println(stringContainsVowels("TV")); // false
-	//
-	// }
-	//
-	// public static boolean stringContainsVowels(String input) {
-	//
-	// return input.toLowerCase().matches(".*[aeiou].*");
-	//
-	// }
+public class CountVowelConsonant {
 
 	public static void main(String[] args) {
 		System.out.println();
-		String s1 = "gauri deulkar";
-		int countv = 0;
-		int countc = 0;
-		for (int i = 0; i < s1.length(); i++) {
-			if (s1.charAt(i) == 'a' || s1.charAt(i) == 'e' || s1.charAt(i) == 'i' || s1.charAt(i) == 'o'
-					|| s1.charAt(i) == 'u') {
-				countv++;
+		String str = "gkear";
+		int count = 0;
+
+		Set<Character> set = new HashSet<Character>();
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (isVowel(c)) {
+				count++;
+				set.add(c);
 			}
-			// // or you can use ifs1.charAt(i) >= 'a' && s1.charAt(i)<='z') in else if
-			// condition
-			//
-			else if (s1.charAt(i) != 'a' || s1.charAt(i) != 'e' || s1.charAt(i) != 'i' || s1.charAt(i) != 'o'
-					|| s1.charAt(i) != 'u') {
-				countc++;
-			}
+
 		}
-		System.out.println("vowels present in string are = " + countv);
-		System.out.println("consonent present in string are = " + countc);
+		System.out.println(" total vowels are :-  " + count);
+		System.out.println("Vowels are:    ");
+		for (Character c : set) {
+			System.out.print(" " + c);
+		}
 
 	}
+
+	public static boolean isVowel(char character) {
+
+		if (character == 'a' || character == 'A' || character == 'e' || character == 'E' || character == 'i'
+				|| character == 'I' || character == 'o' || character == 'O' || character == 'u' || character == 'U') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// int countv = 0;
+	// int countc = 0;
+	// for (int i = 0; i < s1.length(); i++) {
+	// if (s1.charAt(i) == 'a' || s1.charAt(i) == 'e' || s1.charAt(i) == 'i' ||
+	// s1.charAt(i) == 'o'
+	// || s1.charAt(i) == 'u') {
+	// countv++;
+	// System.out.println(s1);
+	// }
+	// // // or you can use ifs1.charAt(i) >= 'a' && s1.charAt(i)<='z') in else if
+	// // condition
+	// //
+	// else if (s1.charAt(i) != 'a' || s1.charAt(i) != 'e' || s1.charAt(i) != 'i' ||
+	// s1.charAt(i) != 'o'
+	// || s1.charAt(i) != 'u') {
+	// countc++;
+	// }
+	// }
+	// System.out.println("vowels present in string are = " + countv);
+	// System.out.println("consonent present in string are = " + countc);
+
 }
